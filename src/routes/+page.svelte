25c87b3b260a1bd9e31d6dff9535e6068c1dc638
Doug Schultz <douglasschultz1@gmail.com>
2024-07-20 17:32:23 -0400

    skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Hi, I'm Doug</h1>
+<p>
+  I'm a software engineer from Rochester NY.
+  I like to learn new technologies and try to build interesting things with them.
+
+  I believe solutions shouldn't be complex or difficult to understand, the best solutions are elegant and easy to maintain.
+</p>
---
+++ src/routes/uses/+page.svelte
@@ -0,0 +1,57 @@
+<h1>Uses</h1>
+<h2>Hardware</h2>
+<h3>Laptop</h3>
+<h4>System76 Lemur Pro</h4>
+My machine for personal development projects.
+<ul>
+  <li>
+    Pop!_OS
+  </li>
+  <li>
+    Memory: 16.0 GiB
+  </li>
+  <li>
+    Processor: 13th Gen Intel® Core™ i7-1355U x 12
+  </li>
+</ul>
+<h3>Desktop</h3>
+The gaming rig.
+<ul>
+  <li>
+    Windows 11
+  </li>
+  <li>
+    AMD Ryzen 5 5600X 3.7 GHz 6-Core Processor
+  </li>
+  <li>
+    MSI MPG B550 GAMING PLUS ATX AM4 Motherboard
+  </li>
+  <li>
+    G.Skill Ripjaws V 64 GB (2 x 32 GB) DDR4-3600 CL18 Memory
+  </li>
+  <li>
+    Samsung 970 Evo Plus 2 TB M.2-2280 PCIe 3.0 X4 NVME Solid State Drive
+  </li>
+  <li>
+    EVGA XC3 ULTRA GAMING GeForce RTX 3090 24 GB Video Card
+  </li>
+  <li>
+    LG 24MC57HQ-P 23.8" 1920 x 1080 60 Hz Monitor
+  </li>
+  <li>
+    LG 27GN950-B 27.0" 3840 x 2160 144 Hz Monitor
+  </li>
+</ul>
+<h2>Editor/Terminal</h2>
+<li>
+  Visual Studio Code
+</li>
+<li>
+  Wezterm
+</li>
+<li>
+  Starship
+</li>
+<li>
+  Currently enjoying Catpuccin Mocha theme for VS Code and Wezterm
+</li>
