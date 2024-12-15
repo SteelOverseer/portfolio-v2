716c0cc892ccd296c0ceb3bffa538351aac8de25
Doug Schultz <douglasschultz1@gmail.com>
2024-12-15 14:48:40 -0500

    finish migration to 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,43 @@
-<!-- @migration-task Error while migrating Svelte code: `<body>` cannot be a child of `<div>`. The browser will 'repair' the HTML (by moving, removing, or inserting elements) which breaks Svelte's assumptions about the structure of your components.
-https://svelte.dev/e/node_invalid_placement -->
 <script>
   import destiny from '$lib/images/destiny.webp'
   import factorio from '$lib/images/factorio.webp'
   import valheim from '$lib/images/valheim.webp'
 </script>
 
-<body>
-  <div id="about">
-    <div>
-      <h2>Hi, I'm Doug</h2>
-    </div>
-    <div>
-      <p>
-        I'm a software engineer that enjoys building cool things.
-        <br><br>
-        By day, I work full time as a full stack software engineer for a local billing company.
-        <br>
-        By night, I contract on the side for a friend who is starting his own tech company, <a href="https://roctechworks.com/#/" target="_blank">roctechworks</a>.
-        <br>
-      </p>
-
-      <h3>The Fun Stuff</h3>
-      I've been an avid gamer my entire life, a hobby that started while I was young playing the Super Nintendo with my family and has evolved into playing 
-      a variety of games on my custom built gaming pc. Steam is my game store of choice, with my top 3 games being:
-      <br>
-      <div id="steam-games">
-        <img src="{destiny}" alt="Destiny 2 - 1,136.7 hours">
-        <img src="{factorio}" alt="Factorio - 357.1 hours">
-        <img src="{valheim}" alt="Valheim - 234.3 hours">
-      </div>
-      
-      Lately I've been playing Elden Ring, wish me luck!
-      <br>
-      And, although I play on PC nowadays, Nintendo still holds my favorite titles in Legend of Zelda, Metroid Prime, and Super Smash Bros.
-      <br>
-      <br>
-      If it's not video games, its magic cards. I enjoy collecting and playing Magic: the Gathering and meet with my friends weekly to terrorize them with a new brew.
-      <br>
-      <br>
-    </div>
+<div id="about">
+  <div>
+    <h2>Hi, I'm Doug</h2>
   </div>
-</body>
+  <div>
+    <p>
+      I'm a software engineer that enjoys building cool things.
+      <br><br>
+      By day, I work full time as a full stack software engineer for a local billing company.
+      <br>
+      By night, I contract on the side for a friend who is starting his own tech company, <a href="https://roctechworks.com/#/" target="_blank">roctechworks</a>.
+      <br>
+    </p>
+
+    <h3>The Fun Stuff</h3>
+    I've been an avid gamer my entire life, a hobby that started while I was young playing the Super Nintendo with my family and has evolved into playing 
+    a variety of games on my custom built gaming pc. Steam is my game store of choice, with my top 3 games being:
+    <br>
+    <div id="steam-games">
+      <img src="{destiny}" alt="Destiny 2 - 1,136.7 hours">
+      <img src="{factorio}" alt="Factorio - 357.1 hours">
+      <img src="{valheim}" alt="Valheim - 234.3 hours">
+    </div>
+    
+    Lately I've been playing Elden Ring, wish me luck!
+    <br>
+    And, although I play on PC nowadays, Nintendo still holds my favorite titles in Legend of Zelda, Metroid Prime, and Super Smash Bros.
+    <br>
+    <br>
+    If it's not video games, its magic cards. I enjoy collecting and playing Magic: the Gathering and meet with my friends weekly to terrorize them with a new brew.
+    <br>
+    <br>
+  </div>
+</div>
 
 <style lang="scss">
   #steam-games {
--- src/routes/uses/+page.svelte
+++ src/routes/uses/+page.svelte
@@ -1,76 +1,72 @@
-<!-- @migration-task Error while migrating Svelte code: `<body>` cannot be a child of `<div>`. The browser will 'repair' the HTML (by moving, removing, or inserting elements) which breaks Svelte's assumptions about the structure of your components.
-https://svelte.dev/e/node_invalid_placement -->
-<body>
-  <div id="uses">
+<div id="uses">
   <h1>Hardware</h1>
-    <hr>
-    <h2>Development</h2>
-    <p>
-      For development, I use a <a href="https://system76.com/laptops/lemur" target="_blank">System76 Lemur Pro</a> laptop.
-    </p>
-    Technical Specs:
-    <br>
-    <table>
-      <tbody>
-        <tr>
-          <td>Memory:</td>
-          <td>16.0 GiB</td>
-        </tr>
-        <tr>
-          <td>Processor:</td>
-          <td>13th Gen Intel® Core™ i7-1355U x 12</td>
-        </tr>
-      </tbody>
-    </table>
-    <p>
-      While at my desk, my keyboard is a <a href="https://dygma.com/pages/defy" target="_blank">Dygma Defy</a> split keyboard with Cherry MX Brown switches.
-    </p>
-    <h2>Gaming</h2>
-    <p>I built my own gaming PC with the following:</p>
-    <ul>
-      <li>
-        AMD Ryzen 5 5600X 3.7 GHz 6-Core Processor
-      </li>
-      <li>
-        MSI MPG B550 GAMING PLUS ATX AM4 Motherboard
-      </li>
-      <li>
-        G.Skill Ripjaws V 64 GB (2 x 32 GB) DDR4-3600 CL18 Memory
-      </li>
-      <li>
-        Samsung 970 Evo Plus 2 TB M.2-2280 PCIe 3.0 X4 NVME Solid State Drive
-      </li>
-      <li>
-        EVGA XC3 ULTRA GAMING GeForce RTX 3090 24 GB Video Card
-      </li>
-      <li>
-        LG 24MC57HQ-P 23.8" 1920 x 1080 60 Hz Monitor
-      </li>
-      <li>
-        LG 27GN950-B 27.0" 3840 x 2160 144 Hz Monitor
-      </li>
-    </ul>
-    <h1>Software</h1>
-    <hr>
-    <p>
-      My development machine runs linux, specifically <a href="https://pop.system76.com/" target="_blank">Pop!_OS</a>
-    </p>
-    <h2>Editor/Terminal</h2>
+  <hr>
+  <h2>Development</h2>
+  <p>
+    For development, I use a <a href="https://system76.com/laptops/lemur" target="_blank">System76 Lemur Pro</a> laptop.
+  </p>
+  Technical Specs:
+  <br>
+  <table>
+    <tbody>
+      <tr>
+        <td>Memory:</td>
+        <td>16.0 GiB</td>
+      </tr>
+      <tr>
+        <td>Processor:</td>
+        <td>13th Gen Intel® Core™ i7-1355U x 12</td>
+      </tr>
+    </tbody>
+  </table>
+  <p>
+    While at my desk, my keyboard is a <a href="https://dygma.com/pages/defy" target="_blank">Dygma Defy</a> split keyboard with Cherry MX Brown switches.
+  </p>
+  <h2>Gaming</h2>
+  <p>I built my own gaming PC with the following:</p>
+  <ul>
     <li>
-      Visual Studio Code with extensions for any of the various programming languages I am working in.
+      AMD Ryzen 5 5600X 3.7 GHz 6-Core Processor
     </li>
     <li>
-      <a href="https://wezfurlong.org/wezterm/index.html" target="_blank">Wezterm</a> gets me more features than the basic terminal, such as tabs or split views.
+      MSI MPG B550 GAMING PLUS ATX AM4 Motherboard
     </li>
     <li>
-      <a href="https://starship.rs/" target="_blank">Starship</a> to customize my terminal.
-      Starship
+      G.Skill Ripjaws V 64 GB (2 x 32 GB) DDR4-3600 CL18 Memory
     </li>
     <li>
-      Currently enjoying Catpuccin Mocha theme for VS Code and Wezterm
+      Samsung 970 Evo Plus 2 TB M.2-2280 PCIe 3.0 X4 NVME Solid State Drive
     </li>
-  </div>
-</body>
+    <li>
+      EVGA XC3 ULTRA GAMING GeForce RTX 3090 24 GB Video Card
+    </li>
+    <li>
+      LG 24MC57HQ-P 23.8" 1920 x 1080 60 Hz Monitor
+    </li>
+    <li>
+      LG 27GN950-B 27.0" 3840 x 2160 144 Hz Monitor
+    </li>
+  </ul>
+  <h1>Software</h1>
+  <hr>
+  <p>
+    My development machine runs linux, specifically <a href="https://pop.system76.com/" target="_blank">Pop!_OS</a>
+  </p>
+  <h2>Editor/Terminal</h2>
+  <li>
+    Visual Studio Code with extensions for any of the various programming languages I am working in.
+  </li>
+  <li>
+    <a href="https://wezfurlong.org/wezterm/index.html" target="_blank">Wezterm</a> gets me more features than the basic terminal, such as tabs or split views.
+  </li>
+  <li>
+    <a href="https://starship.rs/" target="_blank">Starship</a> to customize my terminal.
+    Starship
+  </li>
+  <li>
+    Currently enjoying Catpuccin Mocha theme for VS Code and Wezterm
+  </li>
+</div>
 
 <style lang="scss">
   h1 {
