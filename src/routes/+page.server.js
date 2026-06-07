feee60cdb656310d717f60c70bb4a006bafe17b9
Doug Schultz <douglasschultz1@gmail.com>
2026-06-07 13:39:38 -0400

    update to variables

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,10 @@
 import { error } from '@sveltejs/kit';
-import { PRIVATE_STEAM_API_KEY, PRIVATE_STEAM_USER_ID } from '$env/static/private';
+import { STEAM_API_KEY, STEAM_USER_ID } from '$env/static/private';
 
 export const load = async ({ params }) => {
-  const getUserDataURL = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${PRIVATE_STEAM_API_KEY}&steamids=${PRIVATE_STEAM_USER_ID}`
-  const getOwnedGamesURL = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${PRIVATE_STEAM_API_KEY}&steamid=${PRIVATE_STEAM_USER_ID}&format=json&include_appinfo=true`
-  const getRecentlyPlayedURL = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${PRIVATE_STEAM_API_KEY}&steamid=${PRIVATE_STEAM_USER_ID}&format=json&count=1`
+  const getUserDataURL = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${STEAM_USER_ID}`
+  const getOwnedGamesURL = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_API_KEY}&steamid=${STEAM_USER_ID}&format=json&include_appinfo=true`
+  const getRecentlyPlayedURL = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_API_KEY}&steamid=${STEAM_USER_ID}&format=json&count=1`
 
   const [ownedGamesResp, recentlyPlayedResp] = await Promise.all([
     fetch(getOwnedGamesURL),
