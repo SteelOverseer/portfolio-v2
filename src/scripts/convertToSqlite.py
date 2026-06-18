cbfda8acb552dd28b7515f226b63fe8e55805002
Doug Schultz <douglasschultz1@gmail.com>
2026-06-17 21:07:09 -0400

    script to generate sqlite db from oracle cards

---
+++ src/scripts/convertToSqlite.py
@@ -0,0 +1,57 @@
+import sqlite3
+import json
+
+with open("oracle-cards-20260614210315.json", "r", encoding="utf-8") as file:
+  card_data = json.load(file)
+
+connection = sqlite3.connect("oracleCards.db")
+cursor = connection.cursor()
+
+cursor.execute("PRAGMA foreign_keys = ON;")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS cards (
+  id TEXT PRIMARY KEY,
+  name TEXT NOT NULL,
+  mana_cost TEXT,
+  cmc REAL,
+  type_line TEXT,
+  oracle_text TEXT,
+  rarity TEXT,
+  set_name TEXT,
+  tcgplayer_id INTEGER
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS card_images (
+  card_id TEXT PRIMARY KEY,
+  small TEXT,
+  normal TEXT,
+  large TEXT,
+  png TEXT,
+  art_crop TEXT,
+  border_crop TEXT,
+  FOREIGN KEY(card_id) REFERENCES cards(id) ON DELETE CASCADE
+);
+""")
+
+for card in card_data:
+  card_id = card.get("id")
+
+  cursor.execute("""
+    INSERT OR REPLACE INTO cards (id, name, mana_cost, cmc, type_line, oracle_text, rarity, set_name, tcgplayer_id)
+    VALUES (?,?,?,?,?,?,?,?,?)
+  """,(card_id, card.get("name"), card.get("mana_cost"), card.get("cmc"), card.get("type_line"), card.get("oracle_text"),
+       card.get("rarity"), card.get("set_name"), card.get("tcgplayer_id")))
+  
+  image = card.get("image_uris", {})
+  if image:
+    cursor.execute("""
+        INSERT OR REPLACE INTO card_images (card_id, small, normal, large, png, art_crop, border_crop)
+        VALUES (?,?,?,?,?,?,?)
+    """, (card_id, image.get("small"), image.get("normal"), image.get("large"), image.get("png"), image.get("art_crop"), image.get("border_crop")))
+
+connection.commit()
+connection.close()
+print("Database successfully generated")
